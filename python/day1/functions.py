5b63c8ff56732a885ebd174d8def61c91b31c0c7
unknown <Administrator@Asiet-Lab.ASIETDomain.com>
2019-01-22 16:38:09 +0530

    function update

--- python/day1/functions.py
+++ python/day1/functions.py
@@ -5,3 +5,17 @@ def printMe(str):
 printMe("First time")
 printMe("Second time")
 printMe("third time")
+
+#######
+
+def mathMe(num1,num2):
+    sum = num1+num2
+    dif = num1-num2
+    mul = num1*num2
+    return (sum,dif,mul)
+
+num1=int(input("Enter the first number "))
+num2=int(input("Enter the second number"))
+result = mathMe(num1,num2)
+print(result)
+print(type(result))
