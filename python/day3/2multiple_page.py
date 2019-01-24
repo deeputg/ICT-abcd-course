0ad4de198cfd34de6c1932710b298add8b5e9c53
unknown <Administrator@Asiet-Lab.ASIETDomain.com>
2019-01-24 16:03:50 +0530

    3rdday

---
+++ python/day3/2multiple_page.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Home page"
+
+@app.route('/about')
+def aboutUs():
+    return "Aboutus page"
+
+if __name__=='__main__':
+    app.run()
